28216e2f1872c404b224aa9ca005b6dd516e133f
Haftamu Desta <haftamudesta>
2026-03-18 02:28:17 -0700

    Add initial configuration

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+//configure express and middleware
+
+const express=require("express")
+
+const app=express()
+const cors=require("cors")
+const bodyParser=require("body-parser")
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+module.exports=app
