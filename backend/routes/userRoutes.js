d9aea92cd4b99fa9c8647dcf58a350dcbc7feb4d
Haftamu Desta <haftamudesta>
2026-03-20 12:50:05 -0700

    Add user routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {protect}  = require("../middleware/Protect");
+const authorizeRoles=require("../middleware/authorizeRoles")
+const {
+    getProfile,
+    updateProfile,
+    getAllUsers,
+    deleteUser
+} = require("../controllers/userController");
+
+// Protected routes (require authentication)
+router.use(protect); // All routes below require login
+
+router.get("/profile", getProfile);
+router.put("/profile", updateProfile);
+
+// Admin only routes
+router.use(authorizeRoles("admin")); // All routes below require admin role
+router.get("/get_all_users", getAllUsers);
+router.delete("/delete_user/:id", deleteUser);
