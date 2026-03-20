038240c0daa439bd6252a8130756a32e678edccf
Haftamu Desta <haftamudesta>
2026-03-20 00:13:51 -0700

    Add login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,19 +1,21 @@
 const User=require("../models/user");
 const jwt=require("jsonwebtoken");
-const catchAsychErrors=require("../middleware/catchAsychErrors")
+const catchAsychErrors=require("../middleware/catchAsychErrors") 
+const AppError=require("../utils/errorHandler")
 
 exports.signUp=catchAsychErrors(async(req,res)=>{
         const { name, email, phone, password,confirmPassword } = req.body;
+
+        if (!name || !email || !phone || !password || !confirmPassword) {
+        return next(new AppError("Please provide all required fields", 400));
+    }
 
         const existingUser = await User.findOne({ 
             $or: [{ email }, { phone }] 
         });
 
         if (existingUser) {
-            return res.status(400).json({
-                success: false,
-                message: "User already exists with this email or phone"
-            });
+            return next(new AppError(`User already exists with this ${existingUser.email}`, 400));
         }
         const user = await User.create({
             name,
@@ -26,4 +28,25 @@ exports.signUp=catchAsychErrors(async(req,res)=>{
         sendToken(user, 201, res);
     
 })
+
+exports.login = catchAsychErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new AppError("Please provide email and password", 400));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return next(new AppError("Invalid email or password", 401));
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new AppError("Invalid email or password", 401));
+    }
+
+    sendToken(user, 200, res);
+});
 
