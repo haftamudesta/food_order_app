f9ef63e6431ddc87bc3d818b3ac543ea05f625cf
Haftamu Desta <haftamudesta>
2026-03-20 12:54:34 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
 //configure express and middleware
-
 const express=require("express")
 const app=express()
 const cors=require("cors")
 const bodyParser=require("body-parser")
+//import routes
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
