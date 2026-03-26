bc4610c0ecdbd9fdea1a510443b5a9dcf0ee5edf
Haftamu Desta <haftamudesta>
2026-03-26 05:34:52 -0700

    Add git single order functionality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,3 +6,15 @@ const FoodItem = require("../models/FoodItem");
 const Restaurant = require("../models/restaurant");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const AppError = require("../utils/errorHandler");
+
+
+exports.getSingleOrder=catchAsyncErrors(async(req,res,next)=>{
+    const order=await Order.findById(req.params.id).populate("user","name email").populate("restaurant").exec()//populate=join data from other collections
+    if (!order) {
+            return next(new AppError("Order not found", 404));
+        }
+    res.status(200).json({
+        success:true,
+        order,
+    })
+})
