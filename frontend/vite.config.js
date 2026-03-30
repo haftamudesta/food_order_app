557a5015cf59b633234434647b2e5d82ecd744a0
Haftamu Desta <haftamudesta>
2026-03-30 13:04:33 -0700

    Update vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tsconfigPaths()
   ],
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:8000',
+        changeOrigin:true,
+        secure:false
+      }
+    }
+  }
 })
