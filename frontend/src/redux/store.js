c204b1dafab7416f275e96a5ebafa73d12e0b765
Haftamu Desta <haftamudesta>
2026-04-08 03:59:03 -0700

    Add food reducer to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import restaurantReducer from "./slices/restaurantSlice"
 import userReducer from "./slices/userSlice"
+import foodReducer from "./slices/foodSlice";
 
 const store=configureStore({
     reducer:{
         restaurants:restaurantReducer,
         user:userReducer,
+        food: foodReducer,
     }
 })
 
