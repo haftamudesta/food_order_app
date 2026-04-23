9c549158355ac3b004c187c076bc1b55e373b34a
Haftamu Desta <haftamudesta>
2026-04-23 02:22:17 -0700

    Update axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,8 +4,36 @@ import qs from 'qs';
 const axiosInstance = axios.create({
   baseURL: '/api',
   withCredentials:true,
-  // Qs configuration for params serialization
   paramsSerializer: (params) => qs.stringify(params,{arrayFormat:'repeat'})
 })
+// Request interceptor to add token to headers
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+      console.log("Token added to request:", token.substring(0, 20) + "...");
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor to handle token expiration
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/sign-in";
+    }
+    return Promise.reject(error);
+  }
+);
+
 
 export default axiosInstance
